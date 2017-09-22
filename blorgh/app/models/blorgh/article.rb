6d0e636e673f1220a35aee47f4db1450aeb649b8
Mirv <reski.raptor@gmail.com>
2017-09-22 17:56:13 +0000

    cleaning out comments

--- blorgh/app/models/blorgh/article.rb
+++ blorgh/app/models/blorgh/article.rb
@@ -5,30 +5,16 @@ module Blorgh
     
     attr_accessor :author_name
     
-    # belongs_to :author, class_name: "User"
-    # belongs_to :author, class_name: Blorgh.author_class
     belongs_to :author, class_name: Blorgh.author_class.to_s
    
     # before_validation :set_author # supposedly correct version
     before_validation do self.author = Blorgh.author_class.find_or_create_by(name: author_name) end
-    # before_validation User.find_or_create_by(name: author_name)
-    # before_validation User.find_or_create_by(name: self.author_name)
-    # before_validation do  User.find_or_create_by(name: self.author_name) end
-    
     has_many :comments
   end
   
   
   private
     def set_author
-      # self.author = User.find_or_create_by(name: author_name)
-      # self.author = Blorgh.author_class.constantize.find_or_create_by(name: author_name)
       self.author = Blorgh.author_class.find_or_create_by(name: author_name)
     end
-  # private
-  #   def set_author
-  #     self.author_name = User.find_or_create_by(name: author_name)
-  #     # author = User.find_or_create_by(name: :author_name)
-  #     # author = User.find_or_create_by(name: @author_name)
-  #   end
 end
