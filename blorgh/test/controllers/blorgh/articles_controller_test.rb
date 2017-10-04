451e43c6ff28237b7238bbdd5506379ab4bce43c
Mirv <reski.raptor@gmail.com>
2017-10-04 12:00:16 +0000

    resolved all generic rails engine tests

--- blorgh/test/controllers/blorgh/articles_controller_test.rb
+++ blorgh/test/controllers/blorgh/articles_controller_test.rb
@@ -5,10 +5,8 @@ module Blorgh
     include Engine.routes.url_helpers
 
     setup do
-      # @article = Article.new()
       @routes = Blorgh::Engine.routes
-
-      # puts "Routes:  #{@routes.Rails::Engine.subclasses}"
+      @article = Blorgh::Article.create!(title: "MYSTRING")
     end
 
     test "should get index" do
@@ -23,15 +21,7 @@ module Blorgh
 
     test "should create article" do
       @article = Blorgh::Article.new(title: "MYSTRING")
-      # @article = Blorgh::Engine::Article.new(title: MyString, text: MyText)
-      # @article = Blorgh::Engine::Articles.new(title: MyString, text: MyText)
-      # @article = Blorgh::Articles.new(title: MyString, text: MyText)
-      # @article = Blorgh::Article.new(title: MyString, text: MyText)
-      # @article = Engine::Blorgh::Article.new(title: MyString, text: MyText)
-      # @article = Engines::Blorgh::Article.new(title: MyString, text: MyText)
-      # @article = Blorgh::Article.new() # works
-      # @article = Blorgh::Article.new(title: Test) # same error as first line/original
-
+      
       assert_difference('Article.count') do
         post articles_url, params: { article: { text: @article.text, title: @article.title } }
       end
@@ -39,27 +29,27 @@ module Blorgh
       assert_redirected_to article_url(Article.last)
     end
 
-    # test "should show article" do
-    #   get article_url(@article)
-    #   assert_response :success
-    # end
+    test "should show article" do
+    get article_url(@article)
+      assert_response :success
+    end
 
-    # test "should get edit" do
-    #   get edit_article_url(@article)
-    #   assert_response :success
-    # end
+    test "should get edit" do
+      get edit_article_url(@article)
+      assert_response :success
+    end
 
-    # test "should update article" do
-    #   patch article_url(@article), params: { article: { text: @article.text, title: @article.title } }
-    #   assert_redirected_to article_url(@article)
-    # end
+    test "should update article" do
+      patch article_url(@article), params: { article: { text: @article.text, title: @article.title } }
+      assert_redirected_to article_url(@article)
+    end
 
-    # test "should destroy article" do
-    #   assert_difference('Article.count', -1) do
-    #     delete article_url(@article)
-    #   end
+    test "should destroy article" do
+      assert_difference('Article.count', -1) do
+        delete article_url(@article)
+      end
 
-    #   assert_redirected_to articles_url
-    # end
+      assert_redirected_to articles_url
+    end
   end
 end
